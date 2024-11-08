ac0ef9a02409101144b8116a6a51f86d67b32e87
Kai Schillinger <147519465+KaiSchillinger@users.noreply.github.com>
2024-11-08 08:51:24 +0100

    Seitenunterteilung ist angelegt

--- Python/DataKart/home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("Home Seite")
-    st.write("Willkommen auf der Startseite!")
--- Python/DataKart/main.py
+++ Python/DataKart/main.py
@@ -1,16 +1,4 @@
 import streamlit as st
-import home
-import KartStat
-import auswertung
 
-# Sidebar für die Seitenauswahl
-st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Wähle eine Seite", ["Home", "Neue Einträge", "Daten"])
-
-# Seiten basierend auf der Auswahl aufrufen
-if page == "Home":
-    home.show()
-elif page == "Neue Einträge":
-    KartStat.show()
-elif page == "Daten":
-    auswertung.show()
+st.title('MarioKart')
+st.write('Willkommen')
